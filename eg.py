04858b71ba84f04ce712f6781512118f9d30c607
gsy222 <m-13181588@moe-dl.edu.my>
2024-09-14 19:36:03 +0800

    Update eg.py

--- eg.py
+++ eg.py
@@ -7,9 +7,10 @@ def popup():
     y = random.randint(100, 500)  # 随机Y坐标
     root.geometry(f"+{x}+{y}")    # 移动窗口到随机位置
     result = messagebox.askquestion("你要叫我爸吗？")  # 显示“是”或“不是”
-    
+
     if result == 'yes':  # 用户选择 "是"
-        messagebox.showinfo("哎，真乖！")  # 显示一个新消息
+       messagebox.showinfo("再见", "bye bye")  # 显示 "bye bye"
+        root.quit()  # 退出程序
     else:  # 用户选择 "不是"
         messagebox.showinfo("你选择了‘不是’", "别害怕，继续吧！")  # 显示另一个消息
     
@@ -21,5 +22,8 @@ root.withdraw()  # 隐藏主窗口
 # 无限循环弹出
 while True:
     popup()
+
+root.mainloop()
+
 
 root.mainloop()
