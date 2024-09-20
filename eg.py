b4558e1b12d7e176da03ca74601530ab2c230f0c
fujiang520 <m-13181588@moe-dl.edu.my>
2024-09-20 18:13:47 +0800

    Update eg.py

--- eg.py
+++ eg.py
@@ -6,15 +6,15 @@ def popup():
     x = random.randint(100, 700)  # 随机X坐标
     y = random.randint(100, 500)  # 随机Y坐标
     root.geometry(f"+{x}+{y}")    # 移动窗口到随机位置
-    result = messagebox.askquestion("惊喜！", "叫我一声爸")  # 显示“是”或“不是”
+    result = messagebox.askquestion("", "叫我一声爸")  # 显示“是”或“不是”
     
     if result == 'yes':  # 用户选择 "是"
-        messagebox.showinfo("儿子", "哎乖")  # 显示一个新消息
+        messagebox.showinfo("", "哎乖")  # 显示一个新消息
         roop.quit(10)  #退出程序
     else:  # 用户选择 "不是"
-        messagebox.showinfo("可是", "好吧！")  # 显示另一个消息
+        messagebox.showinfo("", "好吧！")  # 显示另一个消息
     
-    messagebox.showinfo("惊喜！", "好吧！哈哈哈")  # 再次显示原来的消息
+    messagebox.showinfo("", "好吧！哈哈哈")  # 再次显示原来的消息
 
 root = tk.Tk()
 root.withdraw()  # 隐藏主窗口
